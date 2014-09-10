90e348773212d487386d51a1fb16061777495dac
Robert Heedt <rojet601@gmail.com>
2014-09-10 17:54:18 +0200

    Sneaky credits

--- client/ActivityBrowser.lua
+++ client/ActivityBrowser.lua
@@ -4,7 +4,7 @@ function ActivityBrowser:__init()
 	self.active = true
 
 	self.window = Window.Create()
-	self.window:SetTitle("Group Activities")
+	self.window:SetTitle("Group Activities by Rojetto")
 	self.window:SetSizeRel(Vector2(0.5, 0.5))
 	self.window:SetPositionRel(Vector2(0.5, 0.5) - self.window:GetSizeRel() / 2)
 	self.window:Subscribe("WindowClosed", self, self.OnWindowClosed)
