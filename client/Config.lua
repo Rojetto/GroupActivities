3a550c4840fd9f89294342b21bc1e841fa0d521c
Robert Heedt <rojet601@gmail.com>
2014-09-14 19:21:35 +0200

    Restructured config

--- client/Config.lua
+++ client/Config.lua
@@ -1,7 +1,7 @@
-Config = {}
-
---Possible values can be found at http://wiki.jc-mp.com/Lua/Client/Key
---default: VirtualKey.F7
-Config.ActivityBrowserKey = VirtualKey.F7
---default: true
-Config.OpenOnJoin = true
+Config = {
+	--Possible values can be found at http://wiki.jc-mp.com/Lua/Client/Key
+	--default: VirtualKey.F7
+	ActivityBrowserKey = VirtualKey.F7,
+	--default: true
+	OpenOnJoin = true	
+}
